2f873c7f1895ed1fd63de2c817e28c535172537f
Rasmus <102213966+rasmus-ob@users.noreply.github.com>
2022-10-06 06:34:19 +0000

    Updated the client.py to work in 2022

--- code/client.py
+++ code/client.py
@@ -1,27 +1,35 @@
 # -*- coding: utf-8 -*-
-import time, getpass, os, urllib2, random
+import time, getpass, os, urllib3, random
+
 disable_logging = False
+
 
 def query_string(msg):
 	return "[+] %s | %s" % (time.strftime("%H:%M:%S"), str(msg))
 
+
 def log(msg):
 	if not disable_logging:
-		print query_string(msg)
+		print(query_string(msg))
+
 
 def push(msg):
 	if not disable_logging:
 		os.write(1, "\r" + query_string(msg))
 
+
 def input(query):
-	return raw_input(query_string(query))
+	return input(query_string(query))
+
 
 def pwinput(query):
 	return getpass.getpass(query_string(query))
 
+
 def ensure_dir(directory):
 	if not os.path.exists(directory):
 		os.makedirs(directory)
+
 
 def download(url, filename):
 	if os.path.exists(filename):
@@ -35,10 +43,12 @@ def download(url, filename):
 			f.close()
 		log("Done downloading %s" % filename)
 
+
 def dump(data):
 	f = open("dump", "w")
 	f.write(data)
 	f.close()
+
 
 def load(filename):
 	f = open(filename, 'r')
@@ -46,6 +56,7 @@ def load(filename):
 	f.close()
 
 	return data
+
 
 def append(filename, data):
 	f = open(filename, 'a')
